926c32c973e8e9f08369491ea5cdcfa5dd953cfa
ahsan1000 <ahsan.codeman@gmail.com>
2024-08-27 17:02:16 +0500

    Context menu added

--- src/components/layout/TreeViewComponent.tsx
+++ src/components/layout/TreeViewComponent.tsx
@@ -4,25 +4,44 @@ import { TreeItem } from '@mui/x-tree-view/TreeItem';
 import Box from '@mui/material/Box';
 import FolderIcon from '@mui/icons-material/Folder';
 import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
+import { ContextMenu, MenuItem, ContextMenuTrigger } from 'react-contextmenu';
+import './custom-context-menu.css';
 
 const CustomTreeItem = ({ node, onFileDoubleClick }) => {
     const renderTree = (nodes) => {
         const isFolder = Array.isArray(nodes.children);
 
         return (
-            <TreeItem
-                key={nodes.id}
-                itemId={nodes.id}
-                label={
-                    <>
-                        {isFolder ? <FolderIcon sx={{ marginRight: 1 }} /> : <InsertDriveFileIcon sx={{ marginRight: 1 }} />}
-                        {nodes.name}
-                    </>
-                }
-                onDoubleClick={() => !isFolder && onFileDoubleClick(nodes.name)}  // Trigger callback on double click
-            >
-                {isFolder && nodes.children.map((node) => renderTree(node))}
-            </TreeItem>
+            <ContextMenuTrigger id={nodes.id} key={nodes.id}>
+                <TreeItem
+                    itemId={nodes.id}
+                    label={
+                        <>
+                            {isFolder ? <FolderIcon sx={{ marginRight: 1 }} /> : <InsertDriveFileIcon sx={{ marginRight: 1 }} />}
+                            {nodes.name}
+                        </>
+                    }
+                    onDoubleClick={() => !isFolder && onFileDoubleClick(nodes.name)}
+                >
+                    {isFolder && nodes.children.map((node) => renderTree(node))}
+                </TreeItem>
+
+                <ContextMenu id={nodes.id} className="custom-context-menu">
+                    {isFolder ? (
+                        <>
+                            <MenuItem className="custom-context-menu-item" onClick={() => console.log(`Create File in ${nodes.name}`)}>Create File</MenuItem>
+                            <MenuItem className="custom-context-menu-item" onClick={() => console.log(`Create Sub-Folder in ${nodes.name}`)}>Create Sub-Folder</MenuItem>
+                            <MenuItem className="custom-context-menu-item" onClick={() => console.log(`Delete ${nodes.name} and Contents`)}>Delete Folder and Contents</MenuItem>
+                        </>
+                    ) : (
+                        <>
+                            <MenuItem className="custom-context-menu-item" onClick={() => console.log(`Open ${nodes.name}`)}>Open</MenuItem>
+                            <MenuItem className="custom-context-menu-item" onClick={() => console.log(`Delete ${nodes.name}`)}>Delete</MenuItem>
+                        </>
+                    )}
+                </ContextMenu>
+
+            </ContextMenuTrigger>
         );
     };
 
@@ -33,7 +52,7 @@ const TreeViewComponent = ({ folderStructure, onFileDoubleClick }) => {
     return (
         <Box
             sx={{
-                maxHeight: 400,
+                maxHeight: 570,
                 overflowY: 'auto',
                 minWidth: 250,
                 padding: '10px',
