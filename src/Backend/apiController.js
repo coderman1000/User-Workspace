3caf186348a36733d18dc1a76fbf8f4dc7e185b3
ahsan1000 <ahsan.codeman@gmail.com>
2024-09-09 09:08:08 +0500

    getColumnValuesByTimeInterval method

--- src/Backend/apiController.js
+++ src/Backend/apiController.js
@@ -219,3 +219,61 @@ exports.getTableAndColumnNames = async (req, res) => {
     common.handleError(res, error);
   }
 };
+
+exports.getColumnValuesByTimeInterval = async (req, res) => {
+  try {
+    const { dbName, collectionName, columns, startTime, endTime } = req.body;
+    const db = mongoose.connection.useDb(dbName);
+
+    // Convert startTime and endTime to ISO string format
+    const startTimeStr = new Date(startTime).toISOString();
+    const endTimeStr = endTime ? new Date(endTime).toISOString() : null;
+
+    // Build the query object based on the presence of endTime
+    let query;
+    if (endTimeStr) {
+      // Case 1: endTime is provided
+      query = {
+        InsertedDateTime: {
+          $gte: startTimeStr,
+          $lte: endTimeStr,
+        },
+      };
+    } else {
+      // Case 2: endTime is not provided
+      query = {
+        InsertedDateTime: {
+          $gte: startTimeStr,
+        },
+      };
+    }
+
+    // Create projection object
+    const projection = columns.reduce(
+      (proj, col) => ({ ...proj, [col]: 1 }),
+      {}
+    );
+    // Include InsertedDateTime and exclude _id from the projection
+    projection.InsertedDateTime = 1;
+    projection._id = 0;
+
+    console.log("Query:", JSON.stringify(query));
+    console.log("Projection:", JSON.stringify(projection));
+
+    // Find documents without applying projection to debug
+    const rawResult = await db.collection(collectionName).find(query).toArray();
+    console.log("Raw Result:", rawResult);
+
+    // Apply projection and find documents
+    const result = await db
+      .collection(collectionName)
+      .find(query)
+      .project(projection)
+      .toArray();
+    console.log("Result:", result);
+
+    res.json(result);
+  } catch (error) {
+    common.handleError(res, error);
+  }
+};
