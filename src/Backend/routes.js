47de1dfc01b0bdfa7aece02bd334a124e6d8772d
ahsan1000 <ahsan.codeman@gmail.com>
2024-09-10 15:15:25 +0500

    routes updated

--- src/Backend/routes.js
+++ src/Backend/routes.js
@@ -4,6 +4,10 @@ const apiController = require("./apiController");
 
 router.post("/saveFolderStructure", apiController.saveFolderStructure);
 router.get("/getFolderStructure", apiController.getFolderStructure);
+router.get("/getFileContent", apiController.getFileContent);
+router.put("/updateFileOrFolder", apiController.updateFileOrFolder);
+router.delete("/deleteFileOrFolder", apiController.deleteFileOrFolder);
+router.post("/createFileOrFolder", apiController.createFileOrFolder);
 
 router.get("/tables/:dbName", apiController.getTableAndColumnNames);
 router.post("/columns/values", apiController.getColumnValuesByTimeInterval);
